33a7857283b59aae0c68520561fe282fede04979
wxyzcctv <2846629226@qq.com>
2021-10-15 09:03:13 +0800

    模块化剪刀石头布游戏

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,4 +1,18 @@
-console.log('start require');
-var lib = require("./lib.js")
-console.log('end require', lib);
-lib.other = "something"
+const game = require('./lib');
+var count = 0;
+
+// 开启一个进程
+process.stdin.on("data", e => {
+    // 监听开启进程之后的输入内容
+    const playerAction = e.toString().trim();
+    // 将输入内容通过game函数逻辑中并记下返回结果
+    const result = game(playerAction);
+    if (result === -1) {
+        count += 1;
+    }
+    if (count === 3) {
+        console.log("你赢了我三次了，太厉害了，不和你玩了");
+        // 脱出进程
+        process.exit();
+    }
+})
--- commonjs/lib.js
+++ commonjs/lib.js
@@ -1,16 +1,30 @@
-console.log("hello geek");
+module.exports = function (playerAction) {
 
-exports.hello = "world";
+    var random = Math.random() * 3;
 
-exports.add = function (a, b) {
-    return a + b;
+    var computerAction;
+    if (random < 1) {
+        computerAction = "剪刀"
+    } else if (random > 2) {
+        computerAction = "布"
+    } else {
+        computerAction = "石头"
+    }
+
+    if (playerAction === computerAction) {
+        console.log('平局');
+        return 0;
+    } else {
+        if (
+            (playerAction === "剪刀" && computerAction === "石头") ||
+            (playerAction === "石头" && computerAction === "布") ||
+            (playerAction === "布" && computerAction === "剪刀")
+        ) {
+            console.log("你输了");
+            return 1;
+        } else {
+            console.log("你赢了");
+            return -1;
+        }
+    }
 }
-exports.geek = { hello: 'world' };
-
-module.exports = function sub(a, b) {
-    return a - b;
-}
-
-setTimeout(() => {
-    console.log(exports);
-}, 2000)
